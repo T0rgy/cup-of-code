d1bd1f0a988be932e37bf8dbb5d837d359ce3449
torgyLabtop <jctorge@gmail.com>
2022-07-25 19:49:42 -0500

    server.js file

---
+++ mern-group-project-3/server/config/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/example_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
