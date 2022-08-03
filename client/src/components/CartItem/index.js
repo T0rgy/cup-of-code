3ff523701f7750101157ee047b6fd7605b063e34
Torgy <jctorge@gmail.com>
2022-08-02 19:49:44 -0500

    stripe client intigration

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 const CartItem = ({ item }) => {
-    const [state, dispatch] = useStoreContext();
+    const [dispatch] = useStoreContext();
 
     const removeFromCart = item => {
         dispatch({
             type: REMOVE_FROM_CART,
             _id: item._id
         });
+        idbPromise('cart', 'delete', { ...item });
     };
 
     const onChange = (e) => {
@@ -19,12 +21,14 @@ const CartItem = ({ item }) => {
                 type: REMOVE_FROM_CART,
                 _id: item._id
             });
+            idbPromise('cart', 'delete', { ...item })
         } else {
             dispatch({
                 type: UPDATE_CART_QUANTITY,
                 _id: item._id,
                 purchaseQuantity: parseInt(value)
             });
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value)});
         }
     }
 
@@ -38,7 +42,7 @@ const CartItem = ({ item }) => {
                 />
             </div>
             <div>
-                <div>{item.price}</div>
+                <div>{item.name}, ${item.price}</div>
                 <div>
                     <span>Qty:</span>
                     <input
