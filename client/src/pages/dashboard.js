93d997653cfa64b8d7418087cee8f159e82f03cb
msamm <msamm93@gmail.com>
2022-08-04 13:00:51 -0500

    dashboard UI

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,18 +1,39 @@
-import { useNavigate } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { GET_USER, GET_ORDER } from '../utils/queries';
-import Auth from '../utils/auth';
-
 const Dashboard = () => {
-    const navigate = useNavigate();
 
 
     
     return (
-        <div className='container bgimagehome'>
-
-            <div className='container'>
-                <h1>Cup of Code</h1>
+        <div className='container-fluid bgimagehome'>
+            
+            <div className='container text-center'>
+                <img className='homefeatureimg' src='../../images/rizky-subagja-IBhJivtPqHQ-unsplash.jpg'/>
+                <h1 className="hometitle">CUP OF CODE</h1>
+                <p className="hometitle">-_-_-_-_-_-_-_-</p>
+                <div className='row homecard'>
+                    <div className='col'>
+                        <h2>FUEL YOUR MORNING</h2>
+                    </div>
+                    <div className='col'>
+                        <img src='../../images/latte.jpeg' />
+                    </div>  
+                </div>
+                <div className='row homecard'>
+                    <div className='col'>
+                        <img src='../../images/crepe-supreme.jpg' />
+                    </div>
+                    <div className='col'>
+                        <h2>SATISFY A CRAVING</h2>
+                    </div>     
+                </div>
+                <div className='row homecard'>
+                    <div className='col'>
+                        <h2>TREAT YOURSELF</h2>
+                    </div>
+                    <div className='col'>
+                        <img src='../../images/doughnut.jpg' />
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
