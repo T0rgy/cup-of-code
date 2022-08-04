d8f87e100dfbc5330f2987ab3dae4ce90640d2df
msamm <msamm93@gmail.com>
2022-08-04 15:27:48 -0500

    fixed console error for about having h1 inside of p

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -10,24 +10,32 @@ function About() {
           src="../../images/A_small_cup_of_coffee.JPG"
           alt="coffee"
         />
-        <p className="about">
+        <div className="about">
           <h1>About US</h1>
-          Our roots begin in 2000 where we started off as a corner store with
-          basic coffee and tea but now moved to have smoothies and other types of
-          drinks. We specialize in our customer service and pride ourselves in
-          giving a unique experience to each customer.
-        </p>
+          <p>
+            Our roots begin in 2000 where we started off as a corner store with
+            basic coffee and tea but now moved to have smoothies and other types of
+            drinks. We specialize in our customer service and pride ourselves in
+            giving a unique experience to each customer.
+          </p>
+        </div>
 
-        <p className="about">
+        <div className="about">
           <h1>Our Coffee</h1>
-          We have coffee from all over the world. We make it fresh with all
-          organic ingredients. You can come watch as we make it in front of you.
-        </p>
-        <p className="about">
+          <p>
+            We have coffee from all over the world. We make it fresh with all
+            organic ingredients. You can come watch as we make it in front of you.
+          </p>
+          
+        </div>
+
+        <div className="about">
           <h1>Our Food</h1>
-          We have food from all over the world. We make it fresh with all
-          organic ingredients. You can come watch as we make it in front of you.
-        </p>
+          <p>
+            We have food from all over the world. We make it fresh with all
+            organic ingredients. You can come watch as we make it in front of you.
+          </p>
+        </div>
       </div>
     </div>
   );
